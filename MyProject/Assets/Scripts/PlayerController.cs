0cce2ecae0ea7c22c4f22b54a393ac5724230dc1
Clara Lyra <ccl37@duke.edu>
2022-10-20 15:43:05 +0100

    added score board

--- MyProject/Assets/Scripts/PlayerController.cs
+++ MyProject/Assets/Scripts/PlayerController.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic; using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using  TMPro;
+
 
 public class PlayerController : MonoBehaviour { 
 	public Vector2 moveValue;
 	public float speed;
-	private  int count;
+	private  int numPickUps = 5;
+	private int count;
+	public TextMeshProUGUI ScoreText;
+	public TextMeshProUGUI WinText;
+
+
 	void Start()
     {
         count =0;
+        WinText.text ="";
+        SetCountText();
     }
 
 	void OnMove(InputValue value) { moveValue = value.Get<Vector2 >();
@@ -20,9 +30,15 @@ public class PlayerController : MonoBehaviour {
 	void OnTriggerEnter(Collider other){
 		if(other.gameObject.tag == "PickUp"){
 			other.gameObject.SetActive(false);
-			count +=1;
-			Debug.Log(count);
+			count ++;
+			SetCountText();
 
+		}
+	}
+	private void SetCountText(){
+		ScoreText.text = "Score: " +  count.ToString();
+		if(count >= numPickUps){
+			WinText.text = "You win!";
 		}
 	}
 }
